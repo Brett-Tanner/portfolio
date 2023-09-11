59247e8eac4fb4dec561ebdd97bde97c8ba91eb5
Brett Tanner <bretttanner171@gmail.com>
2023-09-11 12:29:41 +0900

    Initial Setup

---
+++ astro.config.mjs
@@ -0,0 +1,45 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/noop",
+    },
+  },
+  integrations: [
+    starlight({
+      title: "",
+      logo: {
+        src: "./src/assets/favicon.svg",
+      },
+      customCss: ["./src/tailwind.css"],
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Example Guide",
+              link: "/guides/example/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+});
